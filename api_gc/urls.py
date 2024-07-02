dfcfa606b9b645c79e632726ca05998d87f14ea4
PC <djennaoui.raouf@gmail.com>
2024-07-02 15:03:37 +0200

    init

--- api_gc/urls.py
+++ api_gc/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('adddqe/',AddDQE.as_view()),
     path('getimg/',ListImages.as_view()),
 
+
     path('delbl/',DeleteBL.as_view()),
     path('getunite/',GetUnites.as_view()),
     path('getprod/',GetProds.as_view()),
