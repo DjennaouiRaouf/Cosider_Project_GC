3936989066df2b65272995538415c02f7f99bf3f
PC <djennaoui.raouf@gmail.com>
2024-07-25 15:19:12 +0200

    init

--- api_gc/models.py
+++ api_gc/models.py
@@ -141,6 +141,7 @@ class Config(models.Model):
 
 
 
+
 class InfoEntr(models.Model):
     Raison_s=models.CharField(max_length=500,null=True,blank=True)
     Nature=models.CharField(max_length=500,null=True,blank=True)
